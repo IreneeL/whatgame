b5604da115a475433f6ffbf4650e7a3b766214af
IreneeL <yl6447@nyu.edu>
2020-04-20 22:19:45 -0400

    final

--- game/sketch.js
+++ game/sketch.js
@@ -8,18 +8,29 @@
 
 // A random word
 
-
 function setup(){
-  noCanvas();
+  createCanvas(windowWidth, windowHeight);
+  background(255);
   let n = floor(random(10)) + 1;
-  let n1 = n-1;
-  let words = ['1111111', '22222', '33', '4', '5','6','7','8','9','10'];
-  textSize(50);
-  text(words[n-1], 300, 150);
-  fill(0, 102, 153);
   let img = createImg('pic/' + n + '.jpg');
-  img.size(900,480);
-  img.position(300,150);
+  img.size(1100,680);
+  img.position(200,170);
 
+  let words = ['emmmm..what about Animal Crossing? Have you pay off your mortgage yet?', 'nah. No gaming today, lets take a day and read a book', 'What Remains of Edith Finch seems good for today. Have you finish discovering Finchs family mystery?', 'Today feels like a silly day---go text your friends and make them play Human Fall Flat together!', 'You still have not finish your giant house constrution in Junyes minecraft server, GO TO WORK.','Time for some quiet and sad moments to calm down from all these things--To The Moon', 'MUuuSE DAssSH!!', 'Overwatch hummmmmmm...', 'Time for PUBG', 'nah, just relax and chill for today.'];
+  textSize(30);
+  textAlign(LEFT);
+  fill(0);
+  text(words[n-1], 300,750,800,800);
 
-}
+  let h = hour();
+  let m = minute();
+  fill(0);
+  textSize(23);
+  textFont('Georgia')
+  text('current time: \n'+ h+':'+m,30,300)
+
+  }
+
+  function mouseClicked(){
+
+  }
